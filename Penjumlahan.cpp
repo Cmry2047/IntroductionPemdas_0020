52cdbc5a73db81bc1708ee08e2b24f12144b73ee
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-02-23 11:51:44 +0700

    Membuat Program Penjumlahan

---
+++ Penjumlahan.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int a, b, c ;
+
+int main ()
+{
+    int a, b, c ;
+
+    cout << "Penjumlahan" << endl;
+    //Input Panjang
+    cout << "Masukkan Angka Pertama:";
+    cin >> a;
+
+    //Input Lebar
+    cout << "Masukkan Angka Kedua:";
+    cin >> b;
+
+    //Penghitung
+    c = a + b;
+
+    //Hasil
+    cout << "Hasilnya Adalah:" << c << endl;
+}
